86a8cf8889e50ab71e1ebf13b53966d648d7aae6
Jihyun <ang21icjh@gmail.com>
2021-11-26 19:42:54 +0900

    #2675 문자열_문자열 반복

--- 2675.py
+++ 2675.py
@@ -1,4 +1,4 @@
-for i in range(int(input())):
-    R,S = input().split()
+for i in range(int(input())): # 테스트 케이스 개수
+    R,S = input().split() # 한 문자당 반복 횟수와 문자열
     l = [s*int(R) for s in S]
     print("".join(l))
